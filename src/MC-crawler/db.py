c8e80090144fc98bb719a42209508c4e53d208d7
Urb@n <matiasurbaneja@hotmail.com.ar>
2025-08-09 19:42:51 -0300

    modifico la consulta a la db

--- src/MC-crawler/db.py
+++ src/MC-crawler/db.py
@@ -5,12 +5,11 @@ import servers
 
 conec = sq.connect('servers.db')
 cursor = conec.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS servers(ip PRIMARY KEY,pais TEXT,version TEXT)')
-
+cursor.execute('CREATE TABLE IF NOT EXISTS servers(ip PRIMARY KEY,pais TEXT,version TEXT, fecha TEXT)')
 
 def insertar(dato : tuple):
     try:
-        cursor.execute('INSERT INTO servers VALUES(?,?,?)',dato)
+        cursor.execute('INSERT INTO servers VALUES(?,?,?,?)',dato)
         conec.commit()
 
     except Exception as e: print(f'error al insertar dato {e}')
@@ -21,7 +20,7 @@ def buscar_version(version : str):
 
     print(f'\n[...] se muestra busqueda segun "{version}"\n')
     try:
-        cursor.execute(f'SELECT ip FROM servers WHERE version LIKE ?',(f'%{version}',))
+        cursor.execute(f'SELECT ip FROM servers WHERE version LIKE ? ORDER BY fecha DESC',(f'%{version}',))
         ips = [ip[0] for ip in cursor] # ip:puerto
         servers.mostrar(ips,version)
 
@@ -33,7 +32,7 @@ def buscar_pais(pais : str):
 
     print(f'\n[...] se muestra busqueda segun "{pais}"\n')
     try:
-        cursor.execute(f'SELECT ip FROM servers WHERE pais = ?',(pais,))
+        cursor.execute(f'SELECT ip FROM servers WHERE pais = ? ORDER BY fecha DESC',(pais,))
         ips = [pais[0] for pais in cursor]
         servers.mostrar(ips,porversion=False)
     
