7e78101154380a7ee33df6702f8736b76d88ffe6
Urb@n <matiasurbaneja@hotmail.com.ar>
2025-09-10 21:30:45 -0300

    agrego apartado artistico

--- src/MC-crawler/main.py
+++ src/MC-crawler/main.py
@@ -1,11 +1,31 @@
 from ui import empaquetar
 from db import conec
+from colorama import init
 
+init()
+VIOLETA = '\033[0;95m'
+RESET = '\033[0m '
+FONDO_V = '\033[0;105m'
+AMARILLO = '\033[0;33m'
 
+VERSION = 'V2.0'
 
+LOGO = f'''   *           '     *
+                  *   '            '  *          *
+      _______----_______                          '       *   
+°'==(______( o_(_____( ;)            *    '          '
+           /|\\                *         *  
+          / | \\    {VIOLETA}MC-Crawler {VERSION}{RESET}   *   '    *               
+         /  |  \\   buscador de servers de Minecraft java           
+        /   |   \\      *                        '          *
+       /    |    \\   {FONDO_V}Escrito por => Urb@n{RESET}
+'''
+ADVERTENCIA = f'{AMARILLO}\n[!] advertencia: El antivirus puede bloquear el correcto funcionamiento del programa y/o dar falsos positivos\n{RESET}'
 
 if __name__ == '__main__':
     try:
+        print(LOGO)
+        print(ADVERTENCIA)
         empaquetar()
         conec.close()
     except Exception as e:
