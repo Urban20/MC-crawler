ca57ad71db44f005a5c34e37826671de95ab9ff0
Urb@n <matiasurbaneja@hotmail.com.ar>
2025-10-02 23:26:32 -0300

    elimino barridos de /24

--- src/MC-crawler/gopython.py
+++ src/MC-crawler/gopython.py
@@ -25,7 +25,6 @@ BINARIO = './escan'
 ORACLE = 'https://docs.oracle.com/en-us/iaas/tools/public_ip_ranges.json'
 AMAZON = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
 
-BLOQUES24 = [(64,94,92),(74,112,76),(74,117,200),(199,195,140)]
 
 def ejecutar_bin():
     'automatiza la ejecucion del bin de go'
@@ -38,19 +37,14 @@ def ejecutar_bin():
         print('\n[+] utilizando bloques web\n')
         BLOQUES16 = bloques
     else:
+        print('\n[+] utilizando bloques predefinidos\n')
         BLOQUES16= [(130,61),(54,36),(14,178),(151,80),(50,20),(149,88),
             (54,38),(116,202),(116,203),(136,243),(66,179),(66,248),
             (63,135),(188,34),(188,40),(162,33),(173,240),(15,204),(51,81)
             ,(135,148)] 
 
     try:
-        
-        # para /24
-
-        for n0,n1,n2 in BLOQUES24:
-            subprocess.Popen([BINARIO,'-n0',str(n0),'-n1',str(n1),'-n2',str(n2),'-hl',str(HILOS),'-b24']).wait()
-        
-
+    
         for n0,n1 in BLOQUES16: # parametros para barrido de /16
             com1 = subprocess.Popen([BINARIO,'-n0',str(n0),'-n1',str(n1),'-hl',str(HILOS)])
         com1.wait()
