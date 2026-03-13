3ba6468f428507474c4e1765935156f3020ecb61
Urban20 <173964326+Urban20@users.noreply.github.com>
2026-03-13 11:26:52 -0300

    agrego funcion de validacion de rangos cidr

--- src/MC-crawler/cidr_/escan_flex.py
+++ src/MC-crawler/cidr_/escan_flex.py
@@ -3,8 +3,35 @@ from escaner.gopython import introducir_parametros,interrupcion
 import re
 import cidr_.data
 
+reg16 = cidr_.data.regex16 + '$'
+reg24 = cidr_.data.regex24 + '$'
 
+es_octeto = lambda oct: int(oct) >= 0 and int(oct) <= 255
 
+def es_cidr(cidr : str,octetos = 2): # revisar
+
+    'da True si el reg especificado coincide con un rango'
+
+    if octetos not in (2,3):
+        return False
+
+    if octetos == 2:
+        regex = reg16
+    else:
+        regex = reg24
+
+    r = re.match(regex,cidr)
+
+    if r is None:
+        return False
+    
+    for x in range(1,octetos + 1):
+
+        if not es_octeto(r.group(x)):
+            return False
+        
+    return True
+    
 def procesar_rango(cidr : str):
     '''esta funcion precesa rangos cidr de 16 o 24 bits usando la siguiente notacion:
     
@@ -14,11 +41,8 @@ def procesar_rango(cidr : str):
 
     190.60.20.0/24''' 
 
-    reg16 = cidr_.data.regex16 + '$'
-    reg24 = cidr_.data.regex24 + '$'
-
-    cidr16 = re.match(reg16,cidr)
-    cidr24 = re.match(reg24,cidr)
+    cidr16 = es_cidr(cidr=cidr,octetos=2)
+    cidr24 = es_cidr(cidr=cidr)
 
     if cidr16:
         print('\niniciando escaneo de 16 bits\n')
