fbffcf57209f7f618af664bb4592f08e41a1822b
Urban20 <173964326+Urban20@users.noreply.github.com>
2026-05-03 21:31:54 -0300

    cerrando el socket correctamente (bug pequeño)

--- src/MC-crawler/utilidades/conectividad.py
+++ src/MC-crawler/utilidades/conectividad.py
@@ -2,11 +2,15 @@ import socket
 
 
 def conectividad():
-    for p in (53,443):
-        s = socket.socket()
-        s.settimeout(5)
-        if s.connect_ex(('8.8.8.8',p)) == 0:
-            s.close()
-            return True
     
+    for p in (53,443):
+ 
+        with socket.socket() as s:
+
+            s.settimeout(5)
+
+            if s.connect_ex(('8.8.8.8',p)) == 0:
+                
+                return True
+        
     return False
